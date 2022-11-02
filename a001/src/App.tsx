644890643573e106d45211429f4e2f8728bda367
fireloong <fireloong@foxmail.com>
2022-11-02 17:43:18 +0800

    first commit

--- a001/src/App.tsx
+++ a001/src/App.tsx
@@ -1,17 +1,9 @@
-import * as React from 'react';
 import { runApp, IAppConfig } from 'ice';
-import LocaleProvider from '@/components/LocaleProvider';
-import { getLocale } from '@/utils/locale';
-
-const locale = getLocale();
 
 const appConfig: IAppConfig = {
   app: {
     rootId: 'ice-container',
-    addProvider: ({ children }) => <LocaleProvider locale={locale}>{children}</LocaleProvider>,
-  },
-  router: {
-    type: 'browser',
   },
 };
+
 runApp(appConfig);
