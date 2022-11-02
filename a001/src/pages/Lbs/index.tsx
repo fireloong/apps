7987c9f6fdf0244b98215cec62880914067ec243
fireloong <fireloong@foxmail.com>
2022-11-02 17:06:28 +0800

    first commit

--- a001/src/pages/Lbs/index.tsx
+++ a001/src/pages/Lbs/index.tsx
@@ -1,5 +1,4 @@
 // import { useExternal } from 'ahooks';
-import { Map } from 'react-bmapgl';
 
 const Lbs = () => {
   // useExternal('https://api.map.baidu.com/api?v=3.0&ak=ps2IZMqQvwtEgvqqC3bmmiG5Cf9WG7RP', { type: 'js' });
@@ -11,15 +10,7 @@ const Lbs = () => {
         <dt>&ensp;</dt>
         <dd>&ensp;</dd>
       </dl>
-      <Map
-        style={{ height: 450 }}
-        center={new BMapGL.Point(116.404449, 39.914889)}
-        zoom={12}
-        heading={0}
-        tilt={40}
-        onClick={(e) => console.log(e)}
-        enableScrollWheelZoom
-      />
+      <div>地图显示</div>
     </>
   );
 };
